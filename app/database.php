2e43f66c8e21612cfa3ea8b2f84573f529fa88db
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-31 17:07:36 -0400

    copied rainbowthing's file structure for deploy

--- app/database.php
+++ app/database.php
@@ -1,32 +1,28 @@
 <?php
 
-	function getDb() {
+  function getDb() {
 
-		$raw_url = 'postgres://fzxlldskryzffn:fc7a92eb175bae58664859f4d22df0a6f26c09134072cf23846acc2223d7b9dd@ec2-54-243-60-48.compute-1.amazonaws.com:5432/d26vtivma4uu8s';
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/.env')) {
+      require($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+      $dotenv = new Dotenv\Dotenv($_SERVER['DOCUMENT_ROOT']);
+      $dotenv->load();
+    }
 
-		// if (file_exists('.env')) {
-		// 	require __DIR__ . '/vendor/autoload.php';
-		// 	$dotenv = new Dotenv\Dotenv(__DIR__);
-		// 	$dotenv->load();
-		// }
+    $url = parse_url(getenv("DATABASE_URL"));
 
-		$url = parse_url($raw_url);
+    $db_port = $url['port'];
+    $db_host = $url['host'];
+    $db_user = $url['user'];
+    $db_pass = $url['pass'];
+    $db_name = substr($url['path'], 1);
 
-		// var_dump($url); (shows you the URL breakdown ('host' =>, 'port' =>, etc.) on the localhost)
-
-		$db_port = $url['port'];
-		$db_host = $url['host'];
-		$db_user = $url['user'];
-		$db_pass = $url['pass'];
-		$db_name = substr($url['path'], 1);
-
-		$db = pg_connect(
-		"host=" . $db_host . 
-		" port=" . $db_port . 
-		" dbname=" . $db_name . 
-		" user=" . $db_user . 
-		" password=" . $db_pass);
-		return $db;
-	}
+    $db = pg_connect(
+      "host=" . $db_host .
+      " port=" . $db_port .
+      " dbname=" . $db_name .
+      " user=" . $db_user .
+      " password=" . $db_pass);
+    return $db;
+  }
 
 ?>
--- app/index.php
+++ app/index.php
@@ -7,39 +7,6 @@
 </head>
 <body>
 
-	<?php
-
-		function getDb() {
-
-			$raw_url = 'postgres://fzxlldskryzffn:fc7a92eb175bae58664859f4d22df0a6f26c09134072cf23846acc2223d7b9dd@ec2-54-243-60-48.compute-1.amazonaws.com:5432/d26vtivma4uu8s';
-
-			// if (file_exists('.env')) {
-			// 	require __DIR__ . '/vendor/autoload.php';
-			// 	$dotenv = new Dotenv\Dotenv(__DIR__);
-			// 	$dotenv->load();
-			// }
-
-			$url = parse_url($raw_url);
-
-			// var_dump($url); (shows you the URL breakdown ('host' =>, 'port' =>, etc.) on the localhost)
-
-			$db_port = $url['port'];
-			$db_host = $url['host'];
-			$db_user = $url['user'];
-			$db_pass = $url['pass'];
-			$db_name = substr($url['path'], 1);
-
-			$db = pg_connect(
-			"host=" . $db_host . 
-			" port=" . $db_port . 
-			" dbname=" . $db_name . 
-			" user=" . $db_user . 
-			" password=" . $db_pass);
-			return $db;
-		}
-
-	?>
-
 	<div class="header">
 		<h1>Herobook</h1>
 		<h3><i>Facebook for Superheroes</i></h3>
@@ -49,8 +16,6 @@
 
 	<?php
 	
-		include('database.php');
-
 		function getHeroes() {
 			$request = pg_query(getDb(), "select * from heroes order by id asc");
 			return pg_fetch_all($request);
